2c8675749fcf01bfb7362d71ef07d9735259c940
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-07-30 11:42:46 +0530

    data_ingestion_completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from South_German_Bank.logging import logger
 
-logger.info("Welcome to our custom Log")
+from South_German_Bank.logging import logger
+from South_German_Bank.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
