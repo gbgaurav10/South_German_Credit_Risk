34b536342797ca90578f5911212a5b7bca38aed5
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-07-30 18:17:06 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 
 from South_German_Bank.logging import logger
 from South_German_Bank.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from South_German_Bank.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>> Stage {STAGE_NAME} completed <<<<")
 except Exception as e:
